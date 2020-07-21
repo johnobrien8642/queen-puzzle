47de92d80fee22e34189eb8e966966b4d1e72d91
your name here <johnobrien8642@gmail.com>
2020-07-21 15:04:18 -0400

    Added tests for rows and cols

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -36,4 +36,30 @@ describe Grid do
 
     expect(result).to eq(false)
   end
+
+    it "does not place queen if queen is already in row" do
+    grid = Grid.new(4)
+    grid.place_queen(0, 0)
+
+    result = grid.place_queen(0, 1)
+
+    expect(result).to eq(false)
+  end
+
+  it "does not place queen if queen is already in col" do
+    grid = Grid.new(4)
+    grid.place_queen(0, 0)
+
+    result = grid.place_queen(1, 0)
+
+    expect(result).to eq(false)
+  end
+
+
 end
+
+
+
+
+
+
