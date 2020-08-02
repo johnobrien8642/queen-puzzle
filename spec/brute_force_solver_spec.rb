5924da3227d1981844c11f0c82932574135ce5b9
your name here <johnobrien8642@gmail.com>
2020-08-02 19:13:27 -0400

    Minor grammar change on line 4

--- spec/brute_force_solver_spec.rb
+++ spec/brute_force_solver_spec.rb
@@ -1,9 +1,11 @@
+require_relative 
 require_relative '../lib/brute_force_solver'
 
 describe BruteForceSolver do
-    it "raises error if grid is smaller than 4x4" do
+    it "raises error if grid is smaller than 4" do
         test_grid = BruteForceSolver.new(3)
 
         expect{ test_grid.solve }.to raise_error(RuntimeError, "No solution for grids smaller than 4")
     end
+
 end
